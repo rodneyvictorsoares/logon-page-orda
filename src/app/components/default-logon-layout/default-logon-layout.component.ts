f5d5d7fe97a68748a14f1de077dfd86be8c9fb0d
rodneyvictorsoares <ordabelem@hotmail.com>
2024-08-02 16:26:41 -0300

    feat: Adicionando estilos iniciais

--- src/app/components/default-logon-layout/default-logon-layout.component.ts
+++ src/app/components/default-logon-layout/default-logon-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-logon-layout',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './default-logon-layout.component.scss'
 })
 export class DefaultLogonLayoutComponent {
-
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
 }
--- src/app/pages/logon/logon.component.ts
+++ src/app/pages/logon/logon.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { DefaultLogonLayoutComponent } from '../../components/default-logon-layout/default-logon-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-logon',
   standalone: true,
   imports: [
-    DefaultLogonLayoutComponent
+    DefaultLogonLayoutComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './logon.component.html',
   styleUrl: './logon.component.scss'
 })
 export class LogonComponent {
+  loginForm!: FormGroup;
 
+  constructor(){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
 }
