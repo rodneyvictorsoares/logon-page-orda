88f5b5a78065f34f829328b2dda59e13f979344b
rodneyvictorsoares <ordabelem@hotmail.com>
2024-08-05 19:14:12 -0300

    feat: Criação de logica de login

--- src/app/components/default-logon-layout/default-logon-layout.component.ts
+++ src/app/components/default-logon-layout/default-logon-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-default-logon-layout',
@@ -11,4 +11,16 @@ export class DefaultLogonLayoutComponent {
   @Input() title: string = "";
   @Input() primaryBtnText: string = "";
   @Input() secondaryBtnText: string = "";
+  @Input() disablePrimaryBtn: boolean = true;
+  
+  @Output("submit") onSubmit = new EventEmitter();
+  @Output("navigate") onNavigate = new EventEmitter();
+
+  submit(){
+    this.onSubmit.emit();
+  }
+
+  navigate(){
+    this.onNavigate.emit();
+  }
 }
--- src/app/pages/logon/logon.component.ts
+++ src/app/pages/logon/logon.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { DefaultLogonLayoutComponent } from '../../components/default-logon-layout/default-logon-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LogonInputComponent } from '../../components/logon-input/logon-input.component';
+import { Router } from '@angular/router';
+import { LogonService } from '../../services/logon.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-logon',
@@ -11,16 +14,34 @@ import { LogonInputComponent } from '../../components/logon-input/logon-input.co
     ReactiveFormsModule,
     LogonInputComponent
   ],
+  providers: [
+    LogonService
+  ],
   templateUrl: './logon.component.html',
   styleUrl: './logon.component.scss'
 })
 export class LogonComponent {
   loginForm!: FormGroup;
 
-  constructor(){
+  constructor(
+    private router: Router,
+    private logonService: LogonService,
+    private toastService: ToastrService
+  ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
+  }
+
+  submit(){
+    this.logonService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success("Usuário logado com sucesso!"),
+      error: () => this.toastService.error("Erro ao acessar o sistema! Tente novamente em outro momento.")
+    })
+  }
+
+  navigate(){
+    this.router.navigate(["registrar"])
   }
 }
